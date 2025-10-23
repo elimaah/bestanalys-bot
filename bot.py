a25f0211fad200b792ea2c979a06664f17405fd9
elimaah <elmah7p@gmail.com>
2025-10-23 22:18:08 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام دلنشین جان 🌸 رباتت با موفقیت فعاله!")
+    await update.message.reply_text("سلام دلنشین جان 🌸 رباتت فعاله!")
 
 async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("💰 قیمت تستی بیت‌کوین: 68,520 دلار")
@@ -17,8 +17,10 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("price", price))
 
+    print("Bot started successfully 🎉")
     app.run_polling()
 
 if __name__ == "__main__":
     main()
+
 
